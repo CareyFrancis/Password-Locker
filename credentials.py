be226618a8bd5e6166aebf6de7d724f2983156b3
CareyFrancis <carexfm@gmail.com>
2019-01-30 19:12:25 +0300

    Added a function to save the credentials

--- credentials.py
+++ credentials.py
@@ -2,5 +2,17 @@ class Credentials:
     ''''
     class that generates instances of credentials
     '''
+    pass
     credential_list = []
-   
+    def __init__(self,credential_detail,password,):
+        '''__init__ methods helps us define properties for our objects
+        '''
+        #this is an example of a comment
+        self.credential_detail = credential_detail
+        self.password = password
+    
+    #credential_list = [] # Empty credential list
+    def save_credential(self):
+        '''save_credential method saves credentials objects into list
+        '''
+        Credentials.credential_list.append(self)
