209ced9e9bfd413a834e2e502cba70b5d07c2f97
CareyFrancis <carexfm@gmail.com>
2019-01-30 18:02:34 +0300

    Created TestCredentials class

--- credentials_test.py
+++ credentials_test.py
@@ -1,2 +1,7 @@
 import unittest
 from credentials import Credentials
+
+class TestCredential(unittest.TestCase):
+    '''
+        The test credential class defines test cases for the credential class
+    '''
