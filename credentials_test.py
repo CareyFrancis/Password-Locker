a2377a130a4a41b6d2dd982a5417bfa5a2830388
CareyFrancis <carexfm@gmail.com>
2019-01-30 18:29:54 +0300

    Added a new method to the file

--- credentials_test.py
+++ credentials_test.py
@@ -5,3 +5,9 @@ class TestCredential(unittest.TestCase):
     '''
         The test credential class defines test cases for the credential class
     '''
+
+    def setUp(self):
+        '''
+        The setUp method will run before each test case
+        '''
+        self.new_credential = Credentials('Twitter','fog123')
